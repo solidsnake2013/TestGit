eed9e72c59816ba6abc76a6f26e1b16cb0e1f69e
solidsnake2013 <solidsnake2013xt@gmail.com>
2013-08-23 00:43:18 +0800

    first commit

---
+++ src/com/git/test/main/HelloGit.java
@@ -0,0 +1,14 @@
+package com.git.test.main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Solidsnake
+ * Date: 13-8-22
+ * Time: 下午9:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
