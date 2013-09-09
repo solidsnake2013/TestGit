3045edfe3ec4f81929b2a066a2dce924112b5f9d
solidsnake2013 <solidsnake2013xt@gmail.com>
2013-09-10 00:08:34 +0800

    修改了个文件~试试看

--- src/com/git/test/main/HelloGit.java
+++ src/com/git/test/main/HelloGit.java
@@ -9,6 +9,6 @@ package com.git.test.main;
  */
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git! 世界");
     }
 }
