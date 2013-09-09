710a0c94799f7b5620383ec5c9c1ac93c218986a
solidsnake2013 <solidsnake2013xt@gmail.com>
2013-09-10 00:11:26 +0800

    好玩

---
+++ src/com/git/test/main/HelloGithub.java
@@ -0,0 +1,14 @@
+package com.git.test.main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Solidsnake
+ * Date: 13-9-10
+ * Time: 上午12:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloGithub {
+    public static void main(String[] args) {
+        System.out.println("再次HELLO ！");
+    }
+}
